381d5132212ddf2f1796fb7b0639bc0a92f61d6e
airuike <airuike@gmail.com>
2020-03-01 17:14:40 -0500

    foo

--- dispatch.go
+++ dispatch.go
@@ -7,234 +7,231 @@ import (
 
 const FOREVER time.Duration = 0
 
-type job struct {
+
+enum BlockType {
+	Default,
+	Barrier,
+	Apply
+}
+
+type countedBlockChan struct {
+	c chan<- *Block
+	count uint64
+}
+
+type Block struct {
 	f       func()
 	done    chan struct{}
-	barrier bool
+	type	BlockType
+	cancel	bool
+}
+
+enum QueueType {
+	Async,
+	Serial
 }
 
 type Queue struct {
-	jobs         chan *job
-	pending_cond *sync.Cond
-	pending_lock sync.Mutex
-	pending      []*job
-	executing    sync.WaitGroup
+
+}
+
+//type Queue interface {
+//	AsyncBlock(b *Block)
+//	Async(f func())
+//	SyncBlock(b *Block)
+//	Sync(f func())
+//	AfterBlock(d time.Duration, b *Block)
+//	After(d time.Duration, b *Block)
+//	Apply(f func(iter int))
+//	GetSpecific(key string) (interface{}, bool)
+//	SetSpecific(key string, value interface{})
+//}
+
+type SerialQueue struct {
+
+}
+
+type AsyncQueue struct {
+
 }
 
 type Group struct {
-	wg              sync.WaitGroup
-	yetToLeaveQueue *Queue
+
 }
 
-func (j *job) run() {
-	j.f()
-	close(j.done)
-}
-
-func (j *job) wait() {
-	<-j.done
-}
-
-func newJob(f func()) *job {
-	return &job{
-		f:    f,
-		done: make(chan struct{}),
+func BlockCreate(t BlockType, f func()) *Block {
+	return &Block{
+		f: f,
+		type: t,
 	}
 }
 
-func newBarrierJob(f func()) *job {
-	return &job{
-		f:       f,
-		done:    make(chan struct{}),
-		barrier: true,
+func (b *Block) Cancel() {
+	b.cancel = true
+}
+
+func (b *Block) TestCancel() bool {
+	return b.cancel
+}
+
+func (b *Block) Perform() {
+	if !b.cancel {
+		b.f()
 	}
+	close(b.done)
 }
 
-// Quickly read jobs from the incoming chan and enqueue them.
-func (q *Queue) startJobReceiver() {
-	go func() {
-		for j := range q.jobs {
-			q.pending_lock.Lock()
-			q.pending = append(q.pending, j)
-			q.pending_lock.Unlock()
-			q.pending_cond.Signal()
-		}
-	}()
+func (b *Block) Notify(q *Queue, f func()) {
+	b.Wait()
+	q.Async(f)
 }
 
-// Dequeue jobs and call the handler function on them.
-func (q *Queue) startJobRunner(f func(j *job)) {
-	go func() {
-		for {
-			q.pending_lock.Lock()
-			for len(q.pending) == 0 {
-				q.pending_cond.Wait()
-			}
-
-			job := q.pending[0]
-			q.pending = q.pending[1:]
-			q.pending_lock.Unlock()
-
-			f(job)
-		}
-	}()
-}
-
-// A Queue whose jobs will be executed one-by-one.
-func SerialQueue() *Queue {
-	q := &Queue{
-		jobs:    make(chan *job),
-		pending: make([]*job, 0, 64),
-	}
-	q.pending_cond = sync.NewCond(&q.pending_lock)
-
-	q.startJobReceiver()
-	q.startJobRunner(func(j *job) {
-		j.run()
-		j.wait()
-	})
-
-	return q
-}
-
-// A Queue whose jobs are run with concurrency and in an undeterministic order.
-func AsyncQueue() *Queue {
-	q := &Queue{
-		jobs:    make(chan *job),
-		pending: make([]*job, 0, 64),
-	}
-	q.pending_cond = sync.NewCond(&q.pending_lock)
-
-	q.startJobReceiver()
-	q.startJobRunner(func(j *job) {
-		if j.barrier {
-			// Make sure the currently executing jobs finish before executing the,
-			// barrier and the wait for it to finish.
-			q.executing.Wait()
-			j.run()
-			j.wait()
-		} else {
-			// Track the other asynchronously executing jobs with a WaitGroup.
-			q.executing.Add(1)
-			go func() {
-				j.run()
-				j.wait()
-				q.executing.Done()
-			}()
-		}
-	})
-
-	return q
-}
-
-// Enqueue the job and wait for it to complete.
-func (q *Queue) Sync(f func()) {
-	j := newJob(f)
-	q.enqueue(j)
-	j.wait()
-}
-
-// Enqueue the job and return immediately.
-func (q *Queue) Async(f func()) {
-	j := newJob(f)
-	q.enqueue(j)
-}
-
-// A Group collects jobs so that their combined completion may be waited upon.
-func NewGroup() *Group {
-	return &Group{
-		yetToLeaveQueue: AsyncQueue(),
-	}
-}
-
-func (q *Queue) enqueue(j *job) {
-	q.jobs <- j
-}
-
-// Enqueue the job and wait for it to complete.
-func (g *Group) Sync(q *Queue, f func()) {
-	g.wg.Add(1)
-	j := newJob(func() {
-		f()
-		g.wg.Done()
-	})
-
-	g.yetToLeaveQueue.Async(func() {
-		j.wait()
-	})
-
-	q.enqueue(j)
-	j.wait()
-}
-
-// Enqueue the job and return immediately.
-func (g *Group) Async(q *Queue, f func()) {
-	g.wg.Add(1)
-	j := newJob(func() {
-		f()
-		g.wg.Done()
-	})
-
-	g.yetToLeaveQueue.Async(func() {
-		j.wait()
-	})
-
-	q.enqueue(j)
-}
-
-// Wait for all jobs enqueued by group to complete, returning false if it times out.
-// FOREVER (0) should be used to wait forever.
-func (g *Group) Wait(d time.Duration) bool {
-	c := make(chan struct{})
-	go func() {
-		g.wg.Wait()
-		close(c)
-	}()
-
-	if 0 != d {
-		select {
-		case <-c:
+func (b *Block) Wait(d time.Duration) bool {
+	if d != FOREVER {
+		switch {
+		case <-b.done:
 			return true
 		case <-time.After(d):
 			return false
 		}
 	} else {
-		<-c
+		<-b.done:
 		return true
 	}
 }
 
-// Schedule the job on the specified queue when jobs previously submitted to the
-// group have completed.
-func (g *Group) Notify(q *Queue, f func()) {
-	g.yetToLeaveQueue.AsyncBarrier(func() {
-		q.Async(f)
+func (b *Block) Once(once * sync.Once) {
+	once.Do(b.Wait(FOREVER))
+}
+
+//func QueueCreate(label string, type QueueType) *Queue {
+//	switch type {
+//	case Serial:
+//		return &SerialQueue{}
+//	case Async:
+//		return &AsyncQueue{}
+//	}
+//}
+
+func QueueCreate(label string, type QueueType) *Queue {
+	return &Queue{}
+}
+
+func (q *Queue) AsyncBlock(b *Block) {
+	q.enqueue(b)
+}
+
+func (q *Queue) Async(f func()) {
+	q.AsyncBlock(BlockCreate(Default, f))
+}
+
+func (q *Queue) SyncBlock(b *Block) {
+	q.enqueue(b)
+	b.Wait(FOREVER)
+}
+
+func (q *Queue) Sync(f func()) {
+	q.SyncBlock(BlockCreate(Default, f))
+}
+
+func (q *Queue) AfterBlock(d time.Duration, b *Block) {
+	<-time.After
+	q.AsyncBlock(b)
+}
+
+func (q *Queue) After(d time.Duration, b *Block) {
+	return q.AfterBlock(d, BlockCreate(Default, f))
+}
+
+func (q *Queue) Apply(iterations int, f func(iter int)) {
+	for i := 0; i < iterations; i++ {
+		j := i
+		iterfunc := func() {
+			f(j)
+		}
+		q.Async(BlockCreate(Default, iterfunc)
+	}
+}
+
+func (q *Queue) GetSpecific(key string) (interface{}, bool) {
+	return q.Map().Load(key)
+}
+
+func (q *Queue) SetSpecific(key string, value interface{}) {
+	q.Map().Store(key, value)
+}
+
+func GroupCreate() *Group {
+	return &Group{}
+}
+
+func (g *Group) AsyncBlock(q *Queue, b *Block) {
+	g.enqueue(b)
+	q.enqueue(b)
+}
+
+func (g *Group) Async(q *Queue, f func()) {
+	g.AsyncBlock(q, BlockCreate(Default, f))
+}
+
+func (g *Group) NotifyBlock(q *Queue, b *Block) {
+	g.enqueue(BlockCreate(Barrier, func() {
+		q.AsyncBlock(b)
 	})
 }
 
-// Increment the count of outstanding jobs in the group.
+func (g *Group) Notify(q *Queue, f func()) {
+	g.NotifyBlock(q, BlockCreate(Default, f))
+}
+
+func (g *Group) Wait(d time.Duration) bool {
+	waiter := g.queue.notifyEmpty()
+	if FOREVER != d {
+		select {
+		case <-waiter:
+			return true
+		case <-time.After(d)
+			return false
+		}
+	} else {
+		<-waiter
+		return true
+	}
+}
+
 func (g *Group) Enter() {
-	g.wg.Add(1)
+	c := make(chan struct{})
+	g.enqueue(BlockCreate(Default, func() {
+		<-c
+	})
+	g.entered <- c
 }
 
-// Decrement the count of outstanding jobs in the group.
 func (g *Group) Leave() {
-	g.wg.Done()
+	c := <-g.entered
+	close(c)
 }
 
-// Submit a barrier job to the queue and wait for it to complete.
-// The barrier won't execute until all previously submitted jobs are complete.
-// All subsequently enqueued jobs will wait for the barrier to complete before executing.
-func (q *Queue) SyncBarrier(f func()) {
-	j := newBarrierJob(f)
-	q.enqueue(j)
-	j.wait()
+// abstract it so that it has a counter...
+func (q *Queue) getEnqueuer() *countedBlockChan {
+	return q.enqueueChan.Load()
 }
 
-// Submit a barrier job to the queue and return immediately.
-// The barrier won't execute until all previously submitted jobs are complete.
-// All subsequently enqueued jobs will wait for the barrier to complete before executing.
-func (q *Queue) AsyncBarrier(f func()) {
-	j := newBarrierJob(f)
-	q.enqueue(j)
+func (q *Queue) enqueueChanFull() {
+
 }
+
+func (q *Queue) enqueue(b *Block) uint64 {
+	for {
+		select c := q.getEnqueuer() {
+		case c <- b:
+			return
+		default:
+			q.enqueueChanFull()
+		}
+	}
+}
+
+func (g *Group) enqueue(b *Block) uint64 {
+}
+
